50dbd28ad6673e9dd60cd11a88f1d8034726eda9
Creamel <lhs739300@gmail.com>
2025-04-03 10:33:39 +0900

    Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
     private CharacterController controller;
+    private NavMeshAgent navAgent;
+    public Animator anim;
+
+    private Vector3 movePoint;
 
     public enum PlayerState
     {
@@ -16,6 +21,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -30,17 +36,33 @@ public class Player : MonoBehaviour
         switch (playerState)
         {
             case PlayerState.Idle:
+
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit) && Input.GetMouseButton(0))
+                {
+                    AnimOn(PlayerState.Move);
+                    movePoint = hit.point;
+                    playerState = PlayerState.Move;
+                }
+
                 break;
             case PlayerState.Move:
+
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit) && Input.GetMouseButton(0))
+                {
+                    movePoint = hit.point;
+                }
+
+                navAgent.SetDestination(movePoint);
+                //playerState = PlayerState.Idle;
                 break;
             case PlayerState.Attack:
                 break;
         }
+    }
 
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit) && Input.GetMouseButton(0))
-        {
-            Debug.Log($"Ray : {hit.point}");
-        }
+    void AnimOn(PlayerState n)
+    {
+        anim.SetInteger("PlayerState", (int)n);
     }
 }
