38529b1afc647012a9b13d47145e5a87a6359f98
Creamel <lhs739300@gmail.com>
2025-05-08 15:43:04 +0900

    Add Enemy Animation & Movement State

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,19 +4,43 @@ public class Enemy : MonoBehaviour
 {
     public enum EnemyState
     {
-        dmddo,
-        dk
+        Idle, Move, Attack, Dead
     }
+    public EnemyState state = EnemyState.Idle;
+    public float searchRange;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //씬 뷰에 기즈모를 그리는 함수
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.DrawWireSphere(transform.position, searchRange);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        EnemyAct();
+    }
+
+    void EnemyAct()
+    {
+        switch (state)
+        {
+            case EnemyState.Idle:
+                if (Vector3.Distance(GameManager.instance.playerTrans.position, transform.position) <= searchRange)
+                {
+                    state = EnemyState.Move;
+                }
+                break;
+            case EnemyState.Move:
+                break;
+        }
+    }
+
+    //에니메이션 실행
+    void AnimOn(int n)
+    {
+        switch (n)
+        {
+            
+        }
     }
 }
